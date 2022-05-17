68c7731e17ccd05b17fc5e94bf9d6ca3b4a40fe7
John Obinna <johnnydoncom@yahoo.com>
2022-05-17 09:58:30 +0100

    account page

--- app/Http/Controllers/Account/AccountController.php
+++ app/Http/Controllers/Account/AccountController.php
@@ -4,15 +4,24 @@ namespace App\Http\Controllers\Account;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Bavix\Wallet\Internal\Service\DatabaseServiceInterface;
+use Illuminate\Support\Facades\Auth;
 
 class AccountController extends Controller
 {
     public function index(){
 
-        $salesEarning = auth()->user()->balance;
+        auth()->user()->wallet->refreshBalance();
+        // auth()->user()->socialWallet->refreshBalance();
+
+        $salesEarning = auth()->user()->wallet->balance;
         $socialEarning = auth()->user()->socialWallet()->balance;
 
-        $allTimeEarning = auth()->user()->balance;
+        $allTimeEarning = Auth::user()->transactions()->where('type', '=', 'deposit')->get()->sum('amount');
+
+        // auth()->user()->socialWallet()->deposit(75);
+
+        // auth()->user()->deposit(50);
 
 
         return view('account.home',[
